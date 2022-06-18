8db211bebbb7afee60616ac385b6f52203b9035d
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-06-18 15:58:29 +0500

    basic03

--- bin/basic_func03.dart
+++ bin/basic_func03.dart
@@ -1,4 +1,9 @@
-// Create a function called func.
+ String func(){
+  return"codeschooluz";
+ }
+//}Create a function called func.
 
 // Return the value "codeschooluz"
-void main() {}
+void main() {
+  print(func());
+}
