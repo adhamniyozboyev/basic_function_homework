6db3b084fe69637c8301b990a9d6ca0675cc511b
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-06-18 16:57:58 +0500

    basic09

--- bin/basic_func09.dart
+++ bin/basic_func09.dart
@@ -1,6 +1,9 @@
+int func(a){
+return a-1;
+}
 //  Create a function called func.
 
 //  Create function arguments a.
 
 //  decrease the value of a to one and return
-void main() {}
+void main() {print(func(6));}
