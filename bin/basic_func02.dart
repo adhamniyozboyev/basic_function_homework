26aa186b1f6edd7e178a613eff9b10d573f0eafe
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-06-18 15:52:15 +0500

    basic02

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,4 +1,8 @@
-//  Create a function called func.
+String func(){
+  return'Hello World';
+}//  Create a function called func.
 
 //  Return the value "Hello World"
-void main() {}
+void main() {
+  print(func());
+}
