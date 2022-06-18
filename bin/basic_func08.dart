b730b7a70318065beeb91870bb3eec2331659202
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-06-18 16:51:03 +0500

    basic08

--- bin/basic_func08.dart
+++ bin/basic_func08.dart
@@ -1,6 +1,9 @@
-//  Create a function called func.
+int func(a){
+  return a+2;
+}//  Create a function called func.
 
 //  Create function arguments a.
 
 //  Increase the value of a to one and return.
-void main() {}
+void main() {print (func(6));
+}
